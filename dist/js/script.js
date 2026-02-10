053edf9de4fa5eae6e8ecceff8716f7f35073b61
ferranodriozola <ferranodriozola@gmail.com>
2026-02-10 01:23:53 +0100

    nova versió de transcripcions

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,18 @@
 const debugLevel = 0; // 0 = Off, 1 = Goatcounter, 2 = Errors, 3 = Logs, 4 = Temps
 
-const VERSIO_TRANSCRIPCIONS = "v.2.7";  // Versió compartida per col_3, col_4 i col_9
+const VERSIO_TRANSCRIPCIONS = "v.2.8";  // Versió de col_3, col_4 i col_9
+const VERSIO_TOTAL = "v.2.8"; // Versió de totes les altres columnes
 
 const VERSIONS_FITXERS = {
-  "col_0.txt": "v.2.7", //paraula
-  "col_1.txt": "v.2.7", //d'on prové
-  "col_2.txt": "v.2.7", //codi
+  "col_0.txt": VERSIO_TOTAL, //paraula
+  "col_1.txt": VERSIO_TOTAL, //d'on prové
+  "col_2.txt": VERSIO_TOTAL, //codi
   "col_3.txt": VERSIO_TRANSCRIPCIONS, //consonant
   "col_4.txt": VERSIO_TRANSCRIPCIONS, //assonant
-  "col_5.txt": "v.2.7", //síl·labes
-  "col_6.txt": "v.2.7", //Vicc
-  "col_7.txt": "v.2.7", //Viq
-  "col_8.txt": "v.2.7", //Diec
+  "col_5.txt": VERSIO_TOTAL, //síl·labes
+  "col_6.txt": VERSIO_TOTAL, //Vicc
+  "col_7.txt": VERSIO_TOTAL, //Viq
+  "col_8.txt": VERSIO_TOTAL, //Diec
   "col_9.txt": VERSIO_TRANSCRIPCIONS //transcripció sencera
   //col_10 - paraula + transcripció
 };

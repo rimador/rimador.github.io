f9a33409bbb08f7815155880cda14ed3c42c6b62
ferranodriozola <ferranodriozola@gmail.com>
2026-02-10 03:19:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const VERSIO_TRANSCRIPCIONS = "v.2.8";  // Versió de col_3, col_4 i col_9
-const VERSIO_TOTAL = "v.2.8"; // Versió de totes les altres columnes
+const VERSIO_TOTAL          = "v.2.8"; // Versió de totes les altres columnes
 
 const VERSIONS_FITXERS = {
   "col_0.txt": VERSIO_TOTAL, //paraula

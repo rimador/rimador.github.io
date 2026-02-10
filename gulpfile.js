2fb00b513a7ed77b842a10cae9768260a6375f63
rimador <157902245+rimador@users.noreply.github.com>
2026-02-10 01:56:18 +0100

    adaptació a workflow
    Ja no vigila tota l'estona, només executa 1 cop

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
 
-// Minificar i combinar CSS
+// Tasca 1: Minificar i combinar CSS
 gulp.task('styles', function () {
     return gulp.src('css/**/*.css', { allowEmpty: true }) 
         .pipe(sourcemaps.init())
@@ -17,7 +17,7 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('dist/css'));
 });
 
-// Minificar JS
+// Tasca 2: Minificar JS
 gulp.task('scripts', function () {
     return gulp.src('js/**/script.js', { allowEmpty: true }) 
         .pipe(sourcemaps.init())
@@ -28,9 +28,5 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('dist/js'));  
 });
 
-gulp.task('watch', function () {
-    gulp.watch('css/**/*.css', gulp.series('styles'));
-    gulp.watch('js/**/script.js', gulp.series('scripts'));
-});
-
-gulp.task('default', gulp.series('styles', 'scripts', 'watch'));
+// Tasca per defecte: Executa styles i scripts, i ACABA.
+gulp.task('default', gulp.series('styles', 'scripts'));

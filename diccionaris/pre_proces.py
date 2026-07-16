8a53ab7713a176c8c2d1743350d9d8c5e90c5f05
Ferran <155725797+ferranodriozola@users.noreply.github.com>
2026-07-16 23:57:18 +0200

    Create pre_proces.py

---
+++ diccionaris/pre_proces.py
@@ -0,0 +1,18 @@
+import glob
+import os
+
+ruta_separat = "diccionaris/separat"
+arxiu_temporal = os.path.join(ruta_separat, "col_10.txt")
+
+patro = os.path.join(ruta_separat, "col_10_*.txt")
+fitxers_petits = sorted(glob.glob(patro))
+
+dades_completes = []
+for fitxer in fitxers_petits:
+    with open(fitxer, 'r', encoding='utf-8') as f: dades_completes.extend(f.read().splitlines())
+
+with open(arxiu_temporal, 'w', encoding='utf-8') as f:
+    f.write("\n".join(dades_completes) + "\n")
+
+print(f"S'han unit {len(fitxers_petits)} fitxers petits.")
+print(f"S'ha creat l'arxiu temporal {arxiu_temporal} amb {len(dades_completes)} paraules.")

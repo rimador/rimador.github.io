2f818ba467978a12238177733317ee4bcd2a9f1b
ferranodriozola <ferranodriozola@gmail.com>
2026-07-21 12:16:29 +0200

    Update pre_proces.py

--- diccionaris/pre_proces.py
+++ diccionaris/pre_proces.py
@@ -1,15 +1,18 @@
 import glob
 import os
 
-ruta_separat = "diccionaris/separat"
-arxiu_temporal = os.path.join(ruta_separat, "col_10.txt")
+ruta_dicc_separats = "separat"
+arxiu_temporal = os.path.join(ruta_dicc_separats, "col_10provisional.txt")
 
-patro = os.path.join(ruta_separat, "col_10_*.txt")
+print("Iniciant el pre-procés: unint arxius col_10_*.txt...")
+
+patro = os.path.join(ruta_dicc_separats, "col_10_*.txt")
 fitxers_petits = sorted(glob.glob(patro))
 
 dades_completes = []
 for fitxer in fitxers_petits:
-    with open(fitxer, 'r', encoding='utf-8') as f: dades_completes.extend(f.read().splitlines())
+    with open(fitxer, 'r', encoding='utf-8') as f:
+        dades_completes.extend(f.read().splitlines())
 
 with open(arxiu_temporal, 'w', encoding='utf-8') as f:
     f.write("\n".join(dades_completes) + "\n")

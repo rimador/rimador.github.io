8f33f6e5236ed5a5e1c8d820338f01645e2126be
ferranodriozola <ferranodriozola@gmail.com>
2026-07-26 10:04:01 +0200

    path json

--- bot/script.py
+++ bot/script.py
@@ -4,8 +4,8 @@ import os
 import tweepy
 from datetime import datetime
 
-FITXER_RIMES = 'bot/resultat_ordenat_cons.json'          
-FITXER_UTILITZATS = 'bot/publicades.json'  
+FITXER_RIMES = 'resultat_ordenat_cons.json'          
+FITXER_UTILITZATS = 'publicades.json'  
 
 def carregar_json(nom_fitxer):
     if not os.path.exists(nom_fitxer):
